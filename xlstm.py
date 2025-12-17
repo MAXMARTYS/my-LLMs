fddfdb09e02805c887ce5907ff89bad65b3388dd
Maksymilian Martys <makm@candf.com>
2025-12-17 15:42:24 +0100

    xLSTM file template

---
+++ xlstm.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import AutoModel
+from torchinfo import summary
+
+class sLSTM:
+    pass 
+
+class mLSTM:
+    pass
+
+class xLSTM:
+    pass
+
+if __name__=='__main__':
+    print('Everything works!')
