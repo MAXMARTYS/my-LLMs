38472f254e9a63c431ec6b8d76baeff035a14b6c
MAXMARTYS <max.martys@gmail.com>
2026-03-04 12:42:05 +0100

    xlstm changes

--- models/xlstm/xlstm.py
+++ models/xlstm/xlstm.py
@@ -4,14 +4,32 @@ import torch.nn.functional as F
 from transformers import AutoModel
 from torchinfo import summary
 
+from utils import TokenEmbedding
 from lstm_blocks import sLSTMblock, mLSTMblock
 
+# Still needs some debugging
 class xLSTM:
-    def __init__(self):
-        pass 
+    def __init__(self, d_model, d_hidden, n_heads, block_types):
+        self.embedding = TokenEmbedding(pretrained=False, embed_dim=d_model)
+        vocab_size, embed_dim = self.embedding.get_info() 
 
-    def forward():
-        pass 
+        assert set(block_types) == set(['s', 'm'])
+
+        self.blocks = nn.ModuleList([
+            mLSTMblock(d_model) if block == 'm' else sLSTMblock(d_model, n_heads) for block in block_types
+        ])
+
+        self.norm = nn.LayerNorm(d_model)
+        self.head = nn.Linear(d_model, vocab_size, bias=False)
+        self.head.weight = self.embedding.embed.weight
+
+    def forward(self, x):
+        x = self.embedding(x)
+        for block in self.blocks:
+            x, _ = block(x)
+        x = self.norm(x)
+        out = self.head(x)
+        return out
 
     @torch.no_grad()
     def generate():
